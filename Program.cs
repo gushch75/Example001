4e3a7e89d98a45bad0a5c8cfabb62cc87babeebc
unknown <gushch75@gmail.com>
2022-04-11 16:19:50 +0300

    создали второй файл

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Hello, World!");
+Console.Write ("Введите ваше имя");
+string username = Console.ReadLine();
+Console.Write ("Привет, ");
+Console.Write (username);
